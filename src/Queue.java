ee4227ae96405ca434be6f9cf97efa3c4f1f2a16
gemg7 <Lightred123>
2019-02-21 23:37:34 -0800

    attempt

---
+++ src/Queue.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Queue<E> {
+
+    private int size;
+    private E[] elements;
+
+//    public Queue() {
+//        elements = new ArrayList<E>[10];
+//        size = 0;
+//    }
+//
+//    void add(E value) {
+//        queue.add(value);
+//    }
+//
+//    E remove() {
+//        return queue.remove();
+//    }
+//
+//    E peek() {
+//        return queue.peek();
+//    }
+//
+//    boolean isEmpty() {
+//        return queue.isEmpty();
+//    }
+//
+//    private void expandSize() {
+//        int increasedSize = queue.size() * 2;
+//        queue = Arrays.copyOf(queue, increasedSize);
+//    }
+//
+//    void remove(int index) {
+//
+//    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
+

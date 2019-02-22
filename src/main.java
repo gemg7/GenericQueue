420f15235e135648d6dcc2433e1f4da2f3830921
gemg7 <Lightred123>
2019-02-22 01:33:35 -0800

    Done

---
+++ src/main.java
@@ -0,0 +1,24 @@
+public class main {
+    public static void main(String[] args) {
+        Queue<Integer> t = new Queue<>();
+
+        t.add(22);
+        t.add(10);
+        t.add(3);
+        t.add(8);
+        t.add(22);
+        t.add(22);
+        t.print();
+        t.remove(2);
+        t.print();
+        t.removeAll(22);
+        t.print();
+        t.add(10);
+        t.add(8);
+        t.add(8);
+        t.add(5);
+        t.print();
+        t.removeDuplicates();
+        t.print();
+    }
+}
